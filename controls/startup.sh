e4c8f91b36e5cc242866c6ebfe795f47c3aee66d
meteodave <54156606+meteodave@users.noreply.github.com>
2022-08-24 16:04:35 -0400

    Delete controls directory

--- controls/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ROOT_DIR=$(HOME)/aerolinux1
-
-modem_check=$(lsusb | grep Qualcomm)
-if [ -z "$modem_check" ]; then
-	$(ROOT_DIR)/cimel_connect/models_connect_and_reset local
-else
-	$(ROOT_DIR)/cimel_connect/models_connect_and_reset hologram
-fi
