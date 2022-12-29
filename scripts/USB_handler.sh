58b837ad1c38badfe499b070f42a1a47c40d6054
ArsenioDev <ArsenioDev@users.noreply.github.com>
2022-12-29 11:06:15 -0500

    fixing of adding conn test

--- scripts/USB_handler.sh
+++ scripts/USB_handler.sh
@@ -5,8 +5,8 @@
 # | Method for resetting USB hub and devices with 3x redundancy |
 # | Script is used for hardware/firmware bugs of Hologram Nova  |
 # | 1)Unbind and rebind the drivers to the hub controller       |
-# | 2)Reset usb using usb_modeswitch			        |
-# | 3)Reset usb using usbreset				        |
+# | 2)Reset usb using usb_modeswitch			                |
+# | 3)Reset usb using usbreset				                    |
 #  =============================================================
 
 now=$(date)
@@ -19,19 +19,13 @@ else
 	true
 fi
 
-
 prodid=$(lsusb | awk '$7=="Terminus"{print $6}' | cut -b 1-4) #get product id string
 vendid=$(lsusb | awk '$7=="Terminus"{print $6}' | cut -b 6-9) #get vendor id string
 
 # Reset hologram modem via command to SOC inside instead of rebinding  
-sudo poff -a 
-#Free up USB port for AT command and kill PPPD session 
+sudo GSM-Down 
 echo -n -e "AT+CFUN=1,1 \r\n" > /dev/ModemCOM
-#ACTUAL RESET HAPPENS (May require recoding of port number)
+#ACTUAL RESET HAPPENS 
 sleep 30
 #Wait for modem to wake back up 
 
-#lsusb | sudo awk '/Terminus.*Hub$/{ system("/usr/bin/usbreset " $6) }'
-#Uses built in debian usbresest command, a custom version is installed in tools
-#User will have to update exeucatable path to this tool
-#sudo /sbin/shutdown -r now
